a3f244007d1ad8799a01d0dc1b3a1601525ae306
khiemhoang <hotvitlonxao@gmail.com>
2026-03-30 10:55:23 +0700

    feat: add GET /me/artist-request endpoint

--- backend/src/infrastructure/routes/user.routes.ts
+++ backend/src/infrastructure/routes/user.routes.ts
@@ -3,4 +3,5 @@ export const userProtectedRoutes = {
     "GET /me": "src/interfaces/http/handlers/users/me.handler",
     "PUT /me": "src/interfaces/http/handlers/users/updateMe.handler",
     "POST /me/artist-request": "src/interfaces/http/handlers/users/artistRequest.handler",
+    "GET /me/artist-request": "src/interfaces/http/handlers/users/myArtistRequest.handler",
 };
